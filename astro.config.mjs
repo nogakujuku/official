df9a369a03d9bdcb046d47a8452b2d7931ee4bb6
fuma <101544710+fumaSuzuki@users.noreply.github.com>
2024-03-10 19:35:32 +0900

    fix css background-image path

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,5 @@ import { defineConfig } from "astro/config";
 export default defineConfig({
   // 現在の作業ディレクトリにある "./foo"ディレクトリを指します。
   root: "dist",
-  // 現在の作業ディレクトリの "./foo/public" ディレクトリを指します。
-  publicDir: "public",
+  publicDir: "dist",
 });
